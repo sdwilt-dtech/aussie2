661d02eacb9d1ce81435aa127f2c70a2b2933840
sdwilt-dtech <75333316+sdwilt-dtech@users.noreply.github.com>
2025-07-08 19:57:59 -0400

    Fix: format code to pass flake8 linting

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,5 +1,6 @@
 def test_subtract():
     assert 5 - 2 == 3
 
+
 def test_multiply():
     assert 3 * 3 == 9
--- tests/test_string.py
+++ tests/test_string.py
@@ -1,5 +1,6 @@
 def test_uppercase():
     assert "hello".upper() == "HELLO"
 
+
 def test_startswith():
     assert "python".startswith("py")
